70bd394d741f97fc34837b76ef8a99003671273d
Suraj Das <surajdas3108@gmail.com>
2023-12-18 17:26:30 +0530

    Added new repository for user repository

---
+++ Restaurant-backend/src/main/java/com/restaurant/repository/UserRestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.restaurant.repository;
+
+import com.restaurant.model.Person_Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRestaurantRepository extends JpaRepository<Person_Restaurant, Integer> {
+
+    List<Person_Restaurant> findAllByRestaurantId(int id);
+}
