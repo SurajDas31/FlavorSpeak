5836b5a8d19e96fe193d4688ee942dc79c0397e1
Suraj Das <surajdas3108@gmail.com>
2023-12-21 17:23:18 +0530

    listed all the user reviews for a restaurant. Reconfigured the code for ratings.

--- Restaurant-Feedback-frontend/src/user/RestaurantDetails.js
+++ Restaurant-Feedback-frontend/src/user/RestaurantDetails.js
@@ -1,56 +1,135 @@
-import { Form, Modal, ProgressBar } from "react-bootstrap";
+import { Button, Form, Modal, ModalBody, ProgressBar } from "react-bootstrap";
+import { REST_URL, getAccessToken } from "../util/AuthUtil";
 import "./RestaurantDetails.css"
+
+import "./RestaurantDetails.css"
+import { useState } from "react";
+import userEvent from "@testing-library/user-event";
 
 const RestaurantDetails = (props) => {
 
+    const [star, setStar] = useState(0);
+
+    const [apiResult, setApiResult] = useState([]);
+
+    const [oneRating, setOneRating] = useState(0);
+    const [twoRating, setTwoRating] = useState(0);
+    const [threeRating, setThreeRating] = useState(0);
+    const [fourRating, setFourRating] = useState(0);
+    const [fiveRating, setFiveRating] = useState(0);
+
+    let loadData = async () => {
+        try {
+            var res = await fetch(REST_URL + "/api/v1/restaurant/review/get/" + props.restaurant.id, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + getAccessToken()
+                }
+            })
+            if (res.status === 200) {
+                let resultData = await res.json()
+                setApiResult(resultData)
+
+                for (const index in resultData) {
+                    const element = resultData[index];
+
+                    if (element.rating === "5") {
+                        setFiveRating(fiveRating + 1)
+                    } else if (element.rating === "4") {
+                        setFourRating(fourRating + 1)
+                    } else if (element.rating === "3") {
+                        setThreeRating(threeRating + 1)
+                    } else if (element.rating === "2") {
+                        setTwoRating(twoRating + 1)
+                    } else if (element.rating === "1") {
+                        setOneRating(oneRating + 1)
+                    }
+
+                }
+
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const ReviewValidation = (value) => {
+        setStar(value);
+        document.getElementById("postReview").disabled = false;
+    }
+
     return (
         <>
-            {console.log(props)}
             <Modal show={props.show}
                 {...props}
                 size="xl"
                 aria-labelledby="contained-modal-title-vcenter"
                 dialogClassName="modal-90h"
-                centered>
-                <Modal.Header closeButton>
+                centered
+                onShow={() => loadData()}
+                onExit={() => { setFiveRating(0); setFourRating(0); setThreeRating(0); setTwoRating(0); setOneRating(0) }}
+            >
+                <Modal.Header>
                     <Modal.Title id="contained-modal-title-vcenter">
                         {props.restaurant.name}
                     </Modal.Title>
-                    <p>
+                    <h5>
                         {props.restaurant.city}, {props.restaurant.state}
-                    </p>
+                    </h5>
                 </Modal.Header>
                 <Modal.Body>
                     <p>
                         {props.restaurant.description}
                     </p>
-
+                    <hr></hr>
                     <div>
-                        <ProgressBar variant="success" now={40} />
-                        <ProgressBar variant="secondary" now={20} />
-                        <ProgressBar variant="info" now={60} />
-                        <ProgressBar variant="warning" now={80} />
-                        <ProgressBar variant="danger" now={50} />
+                        <div className="progress-bar-section">
+                            <div><p>5</p><ProgressBar variant="warning" now={fiveRating * 100 / apiResult.length} label={fiveRating} /></div>
+                            <div><p>4</p><ProgressBar variant="warning" now={fourRating * 100 / apiResult.length} label={fourRating} /></div>
+                            <div><p>3</p><ProgressBar variant="warning" now={threeRating * 100 / apiResult.length} label={threeRating} /></div>
+                            <div><p>2</p><ProgressBar variant="warning" now={twoRating * 100 / apiResult.length} label={twoRating} /></div>
+                            <div><p>1</p><ProgressBar variant="warning" now={oneRating * 100 / apiResult.length} label={oneRating} /></div>
+                        </div>
+                        <div className="average-rating-section">
+                            {Number.parseFloat((5 * fiveRating + 4 * fourRating + 3 * threeRating + 2 * twoRating + 1 * oneRating) / apiResult.length).toFixed(1)}
+                        </div>
+
                     </div>
+
+                    <hr></hr>
+                    <Modal.Body scrollable="true">
+                        <h4>Reviews</h4>
+                        <div className="review-section" >
+                            {apiResult.map(r => {
+                                return (
+                                    <ul key={r.id} className="review-ul">
+                                        <li><span id="profile-name">{r.person.firstName} {r.person.lastName}</span><span id="user-rating">{r.rating}★ </span></li>
+                                        <li>{r.reviews}</li>
+                                    </ul>)
+                            })}
+                        </div>
+                    </Modal.Body>
                 </Modal.Body>
                 <Modal.Footer>
-                    <div class="rate">
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label for="star5" title="text">5 stars</label>
-                        <input type="radio" id="star4" name="rate" value="4" />
-                        <label for="star4" title="text">4 stars</label>
-                        <input type="radio" id="star3" name="rate" value="3" />
-                        <label for="star3" title="text">3 stars</label>
-                        <input type="radio" id="star2" name="rate" value="2" />
-                        <label for="star2" title="text">2 stars</label>
-                        <input type="radio" id="star1" name="rate" value="1" />
-                        <label for="star1" title="text">1 star</label>
-                    </div>
                     <Form.Control
                         as="textarea"
-                        placeholder="Leave a comment here"
+                        placeholder="Leave your review here"
                         style={{ height: '100px' }}
                     />
+                    <div className="rate" >
+                        <input type="radio" id="star5" name="rate" value="5" onClick={(e) => ReviewValidation(e.target.value)} />
+                        <label htmlFor="star5" title="text">5 stars</label>
+                        <input type="radio" id="star4" name="rate" value="4" onClick={(e) => ReviewValidation(e.target.value)} />
+                        <label htmlFor="star4" title="text">4 stars</label>
+                        <input type="radio" id="star3" name="rate" value="3" onClick={(e) => ReviewValidation(e.target.value)} />
+                        <label htmlFor="star3" title="text">3 stars</label>
+                        <input type="radio" id="star2" name="rate" value="2" onClick={(e) => ReviewValidation(e.target.value)} />
+                        <label htmlFor="star2" title="text">2 stars</label>
+                        <input type="radio" id="star1" name="rate" value="1" onClick={(e) => ReviewValidation(e.target.value)} />
+                        <label htmlFor="star1" title="text">1 star</label>
+                    </div>
+                    <Button id="postReview" disabled>Post Review</Button>
+
                 </Modal.Footer>
             </Modal>
         </>
