f1406670055e60030d9c7f792b6819406b9f6c77
Suraj Das <surajdas3108@gmail.com>
2024-01-04 16:38:16 +0530

    created a new validAccessToken method.

--- Restaurant-backend/src/main/java/com/restaurant/controller/AuthController.java
+++ Restaurant-backend/src/main/java/com/restaurant/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.restaurant.controller;
 
-import com.restaurant.model.auth.registerUser.UserRegister;
-import com.restaurant.service.AuthenticationService;
+
 import com.restaurant.model.auth.AuthenticationRequest;
 import com.restaurant.model.auth.AuthenticationResponse;
+import com.restaurant.model.auth.registerUser.UserRegister;
+import com.restaurant.service.AuthenticationService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,14 +30,13 @@ public class AuthController {
     public ResponseEntity<?> doLogin(@RequestBody AuthenticationRequest request) {
         AuthenticationResponse authenticationResponse = new AuthenticationResponse();
         service.login(request, authenticationResponse);
-        System.out.println(authenticationResponse);
-        if(authenticationResponse.getStatus().equals("User not found"))
+        if (authenticationResponse.getStatus().equals("User not found"))
             return new ResponseEntity<>("{\"status\":\"Username/Email id not valid\"}", HttpStatus.BAD_REQUEST);
 
         if (authenticationResponse.getStatus().equals("Bad Credentials")) {
             return new ResponseEntity<>("{\"status\":\"BAD CREDENTIALS\"}", HttpStatus.UNAUTHORIZED);
         }
-        if(authenticationResponse.getStatus().equals("Locked")){
+        if (authenticationResponse.getStatus().equals("Locked")) {
             return new ResponseEntity<>("{\"status\":\"Locked\"}", HttpStatus.LOCKED);
         }
         return ResponseEntity.ok(authenticationResponse);
@@ -54,5 +55,13 @@ public class AuthController {
             return new ResponseEntity<>("INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>("{\"status\":\"User has been created.\"}", HttpStatus.CREATED);
+    }
+
+    @PostMapping("valid/accessToken")
+    public ResponseEntity validAccessToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
+        if (accessToken != null && accessToken.startsWith("Bearer "))
+            accessToken = accessToken.substring(7);
+        boolean validAccessToken = service.validAccessToken(accessToken);
+        return new ResponseEntity("{\"status\":\"" + validAccessToken + "\"}", HttpStatus.OK);
     }
 }
