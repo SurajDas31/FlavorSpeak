eeec8e94967ca93c0911ab867c1c0cc9bdd523a2
Suraj Das <surajdas3108@gmail.com>
2024-01-30 17:15:54 +0530

    New file commit for admin level.

---
+++ Restaurant-Feedback-frontend/src/user/settings/admin/AddRestaurant.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { REST_URL, getAccessToken } from "../../../util/AuthUtil";
+
+import { ShowSuccessMessage } from "../../../util/AlertUtil";
+
+const AddRestaurant = (props) => {
+
+    const [restaurantData, setRestaurantData] = useState({});
+    const [msgToggle, setMsgToggle] = useState(false);
+
+
+    const saveRestaurantData = async (event) => {
+        event.preventDefault();
+
+        try {
+            var res = await fetch(REST_URL + "/api/v1/restaurant/save", {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + getAccessToken(),
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(restaurantData)
+            })
+            if (res.status === 200) {
+                setMsgToggle(true);
+                props.onHide();
+
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    return (
+        <>
+            {ShowSuccessMessage(msgToggle, "Data saved successfully")}
+            <Modal show={props.show}
+                {...props}
+                size="xl"
+                aria-labelledby="contained-modal-title-vcenter"
+                dialogClassName="modal-90h"
+                onExit={() => setRestaurantData({})}
+                centered
+            >
+                <Modal.Header>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Add a Restaurant
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={saveRestaurantData}>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Restaurant Name</Form.Label>
+                            <Form.Control type="text" placeholder="Name" value={restaurantData.name} onChange={(e) => { setRestaurantData((old) => ({ ...old, name: e.target.value })) }} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" placeholder="City" value={restaurantData.city} onChange={(e) => { setRestaurantData((old) => ({ ...old, city: e.target.value })) }} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
+                            <Form.Label>State</Form.Label>
+                            <Form.Control type="text" placeholder="State" value={restaurantData.state} onChange={(e) => { setRestaurantData((old) => ({ ...old, state: e.target.value })) }} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Owner Name</Form.Label>
+                            <Form.Control type="text" placeholder="Owner name" value={restaurantData.ownerName} onChange={(e) => { setRestaurantData((old) => ({ ...old, ownerName: e.target.value })) }} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Mobile No.</Form.Label>
+                            <Form.Control type="text" placeholder="Owner name" value={restaurantData.mobileNo} onChange={(e) => { setRestaurantData((old) => ({ ...old, mobileNo: e.target.value })) }} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control type="text" placeholder="Max 200 character" maxLength={200} value={restaurantData.description} onChange={(e) => { setRestaurantData((old) => ({ ...old, description: e.target.value })) }} />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">Submit</Button>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default AddRestaurant;
