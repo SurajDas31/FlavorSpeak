2f8a3f37f59b0b1c7f048c6b3a06e61f3dc3d032
Suraj Das <surajdas3108@gmail.com>
2024-02-29 16:43:59 +0530

    Redesigned UI with Material UI library

--- Restaurant-Feedback-frontend/src/user/Dashboard.js
+++ Restaurant-Feedback-frontend/src/user/Dashboard.js
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+
 import img1 from "../home/img/Home-splash-1.jpg"
 
 import HomeNavbar from "../navbar/Navbar";
@@ -7,6 +7,8 @@ import { REST_URL, getAccessToken, isLoggedIn } from "../util/AuthUtil";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css"
 import RestaurantDetails from "./RestaurantDetails";
+import { Box, Card, CardActions, CardContent, CardMedia, Typography, Grid } from "@mui/material";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Dashboard = () => {
     const navigate = useNavigate()
@@ -19,7 +21,7 @@ const Dashboard = () => {
         isLoggedIn().then(res => {
             if (!res)
                 navigate("/")
-            }
+        }
         );
 
         loadRestaurantData();
@@ -45,7 +47,7 @@ const Dashboard = () => {
     return (
         <>
             <HomeNavbar />
-            <Container className="mt-5">
+            {/* <Container className="mt-5">
                 <Row xs={2} md={3} lg={4} className="g-4">
                     {
                         restaurantList.map((r) => {
@@ -69,8 +71,40 @@ const Dashboard = () => {
                     }
 
                 </Row>
+               
+            </Container > */}
+            <Box>
+                <Grid container spacing={3} sx={{ mt: '1px', display: "flex", justifyContent: "center" }} >
+                    {restaurantList.map((r) => {
+                        return (
+                            <Grid item key={r.id} >
+                                <Card className="restaurant-card" sx={{ maxWidth: 345, minWidth: 345, maxHeight: 345, minHeight: 345 }} onClick={() => { setModalToggle(true); setRestaurant(r) }}>
+                                    <CardMedia
+                                        sx={{ height: 140 }}
+                                        image={img1}
+                                        title=""
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: '1', WebkitBoxOrient: 'vertical' }}>
+                                            {r.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary" sx={{ height: 80.06, overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: '4', WebkitBoxOrient: 'vertical' }}>
+                                            {r.description}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions sx={{ position: 'relative', bottom: '-15px', justifyContent: 'center' }}>
+                                        <Typography variant="body1">
+                                        <LocationOnIcon/>  {r.city}, {r.state}
+                                        </Typography>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+
                 <RestaurantDetails show={modalToggle} onHide={() => setModalToggle(false)} restaurant={restaurant} />
-            </Container >
+            </Box >
         </>
     );
 }
