8b2c97e33f139f7832190cbe539eef4e385fa38f
Suraj Das <surajdas3108@gmail.com>
2024-05-06 17:12:55 +0530

    File storage service created.

---
+++ Restaurant-backend/src/main/java/com/restaurant/model/person/Person_Image.java
@@ -0,0 +1,83 @@
+package com.restaurant.model.person;
+
+import com.restaurant.model.Person;
+
+import javax.persistence.*;
+
+@Entity
+public class Person_Image {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private long lastModifiedDate = System.currentTimeMillis() / 1000L;
+
+    private String fileName;
+
+    private String fileType;
+
+    private long fileSize;
+
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(long lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
