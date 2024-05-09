0662ddf1b154b4928a8a1d4935ede20ca5287cb2
Suraj Das <54069735+SurajDas31@users.noreply.github.com>
2024-05-09 16:45:09 +0530

    Delete Restaurant-backend directory

--- Restaurant-backend/src/main/java/com/restaurant/service/RestaurantService.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.service;
-
-import com.restaurant.model.restaurant.RestaurantReviewPosting;
-
-public interface RestaurantService {
-
-    void saveReviews(RestaurantReviewPosting restaurantReviewPosting, String authorization);
-}
